56b8ccbfee6f0f245b2e0632ea4bf6c1dac196b7
mhmddabdeltawab21 <mohamedabdeltawab338@gmail.com>
2024-02-29 15:52:36 +0200

    added ablilty to create new items

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,7 +1,11 @@
 const { body, validationResult } = require("express-validator");
 const Items = require("../models/itemModel");
 const expressAsyncHandler = require("express-async-handler");
-const URL = require("node:url").URL
+const Categories = require("../models/categoryModel")
+
+
+
+
 
 const itemList = expressAsyncHandler(async (req, res, next) => {
 	const items = await Items.find().exec();
@@ -46,10 +50,37 @@ const itemUpdatePost = [
     }),
 ];
 
+const itemCreateGet = expressAsyncHandler(async(req,res,next)=>{
+	const categories = await Categories.find().exec()
+	res.render("itemCreate" , {categories:categories})
+})
+
+
+const itemCreatePost = [
+	body("name").trim().isLength({ min: 1 }).escape(),
+	body("price").trim().isNumeric().escape(),
+
+	expressAsyncHandler(async (req, res, next) => {
+		const errors = validationResult(req);
+		
+		const item = new Items()
+		if (errors.isEmpty()) {
+			item.name = req.body.name;
+			item.price = req.body.price;
+			item.category = req.body.category
+			item.stock = req.body.stock;
+			await item.save();
+		}
+
+		res.redirect(item.url);
+	}),
+];
 module.exports = {
 	itemList,
 	itemPage,
 	itemDelete,
 	itemUpdateGet,
-    itemUpdatePost
+    itemUpdatePost,
+	itemCreateGet,
+	itemCreatePost
 };
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { index } = require("../controllers/indexController");
-const { itemList, itemPage, itemDelete, itemUpdateGet, itemUpdatePost } = require("../controllers/itemController");
+const { itemList, itemPage, itemDelete, itemUpdateGet, itemUpdatePost, itemCreateGet, itemCreatePost } = require("../controllers/itemController");
 const router = express.Router();
 
 /* GET home page. */
@@ -9,12 +9,13 @@ router.get("/", index);
 /* Item routes */
 
 router.get("/items" , itemList);
+router.get("/items/create" ,itemCreateGet);
+router.post("/items/create" ,itemCreatePost);
 router.get("/items/:item" ,itemPage);
 router.get("/items/:item/update" , itemUpdateGet);
 router.post("/items/:item/update" , itemUpdatePost)
 router.post("/items/:item/delete" , itemDelete);
-router.get("/items/create");
-router.post("/items/create");
+
 
 
 /* Category routes */
